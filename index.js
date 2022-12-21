63e528e7f2ec4443c6fb5c5afe5d273387b34044
omar kanaan <93411313+0m4rk4n4n@users.noreply.github.com>
2022-12-21 00:23:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ const db=mysql.createConnection(
 err ? res.json(err) : res.json("Book has been deleted successfully!")
         })
     })
-app.listen(8000,()=>
+app.listen(process.env.PORT || 8000,()=>
 {
-    console.log(`Server is listening on port 8000`)
+    console.log(`Server is listening on port ${process.env.PORT || 8000}`)
 })
